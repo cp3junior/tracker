742bbe220f74dcd3f467be474ab40694b8c344ab
RAILALA Andriatsimarivo <r3andrew@gmail.com>
2022-09-21 22:35:28 +0300

    add firebase

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCpt2WSdwUAk8NTW_AkKLj7BkAwOqQEcac",
-  authDomain: "vola-e0195.firebaseapp.com",
-  projectId: "vola-e0195",
-  storageBucket: "vola-e0195.appspot.com",
-  messagingSenderId: "135141418427",
-  appId: "1:135141418427:web:b2007b90d8d46d65b9109e",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
